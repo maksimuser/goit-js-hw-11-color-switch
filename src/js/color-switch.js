3e084d2f5a567c99552aba616fccd6a413e0fc3d
Dema Maksim <maksimdema@gmail.com>
2021-01-22 15:21:19 +0200

    Update color-switch.js

--- src/js/color-switch.js
+++ src/js/color-switch.js
@@ -1,57 +1,63 @@
 const colors = [
-  // '#FFFFFF',
+  '#FFFFFF',
   '#FFFF00',
   '#2196F3',
   '#4CAF50',
   '#FF9800',
   '#009688',
   '#795548',
+  '#004DFF',
+  '#00FF00',
+  '#000000',
+  '#FF00FF',
 ];
+
+let defaultBodyColor = (document.body.style.background = '#858585');
 
 const refs = {
   btnStart: document.querySelector('button[data-action="start"]'),
   btnStop: document.querySelector('button[data-action="stop"]'),
 };
 
-let isActive = null;
-
-refs.btnStart.addEventListener('click', () => {
-  console.log('Start');
-
-  if (isActive) {
-    return;
-  }
-  isActive = true;
-
-  let currentColor = '';
-
-  let intId = setInterval(() => {
-    console.log('change bgc body');
-
-    let currentInd = randomIntegerFromInterval(0, colors.length - 1);
-    console.log(currentInd);
-
-    colors.map((el, ind) => {
-      if (currentInd === ind) {
-        currentColor = el;
-        console.log(currentColor);
-        return currentColor;
-      }
-    });
-
-    refs.btnStop.addEventListener('click', () => {
-      isActive = false;
-
-      clearInterval(intId);
-      document.body.style.background = currentColor;
-
-      console.log('stop');
-    });
-  }, 1000);
-});
-
-function handleBtnStop() {}
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let currentColor = '';
+
+const switchColors = {
+  isActive: false,
+  intervalId: null,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      let currentIndex = randomIntegerFromInterval(0, colors.length - 1);
+
+      colors.map((el, ind) => {
+        if (currentIndex === ind) {
+          document.body.style.background = el;
+          currentColor = el;
+        }
+      });
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    if (!this.isActive) {
+      return;
+    }
+    this.intervalId = null;
+    this.isActive = false;
+
+    defaultBodyColor = currentColor;
+  },
+};
+
+refs.btnStart.addEventListener('click', switchColors.start.bind(switchColors));
+refs.btnStop.addEventListener('click', switchColors.stop.bind(switchColors));
